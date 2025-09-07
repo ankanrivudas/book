7d506bb2f0d230bc82dfc470e309132098c4cfa4
Ankan <ankanrivudas@gmail.com>
2025-09-08 01:28:58 +0530

    'updated'

--- pragati/src/app/page.js
+++ pragati/src/app/page.js
@@ -1,6 +1,26 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import HTMLFlipBook from "react-pageflip";
+
+function MyBook(props) {
+  return (
+    <HTMLFlipBook width={300} height={500}>
+      <div className="demoPage">
+        <Image src="/image1.jpg" alt="Book Cover" width={300} height={500} />
+      </div>
+      <div className="demoPage">
+        <Image src="/image2.jpg" alt="Book Cover" width={300} height={500} />
+      </div>
+    </HTMLFlipBook>
+  );
+}
+
 export default function Home() {
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.page}>
+      <MyBook />
+    </div>
+  );
 }
